0841923b3deab626b760ce21d60b2ce03e9951bb
ali <alidevops1504@gmail.com>
2025-09-11 17:54:50 +0330

    delete folder additional

--- src/infrastracture/providers/user.repository.provider.ts
+++
@@ -1,8 +0,0 @@
-// import { RegisterRepository } from 'src/infrastracture/repository/user.repository';
-// import { DataSource } from 'typeorm';
-
-// export const UserRepositoryProvider = {
-//   provide: 'IUserRepository',
-//   useFactory: (dataSource: DataSource) => new RegisterRepository(DataSource),
-//   inject: [dataSource],
-// };
